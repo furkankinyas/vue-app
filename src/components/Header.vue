ae5534e9fdaca509d3ea68ddb31adc3e4e126c64
furkan kinyas <kinyasfurkan@gmail.com>
2019-05-22 00:24:23 +0300

    title change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="background-primary color-white padding-md height-md flex between middle margin-bottom-md">
     <div class="flex middle">
-    <h1 class="font-weight-7 margin-right-lg">React My App</h1>
+    <h1 class="font-weight-7 margin-right-lg">Vue My App</h1>
     <input @keyup="inputChange" :value="value" @input="updateValue($event.target.value)" class="radius-max height-sm padding-md header-search" placeholder="arama" type="text"/>
     </div>
       <nav>
