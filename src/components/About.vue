ce1d67bf92c1fccc0f4ff5ff982c750e65dc4fed
furkankinyas <kinyasfurkan@gmail.com>
2019-05-21 10:34:42 +0300

    initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header/>
+    <h1>About</h1>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+  components: {
+    Header
+  },
+  name: 'About',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import About from '@/components/About'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  //mode: "history",
+  history: false,
+  root: '/',
+  hashbang: false,
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/about',
+    name: 'About',
+    component: About
+  }]
+})
